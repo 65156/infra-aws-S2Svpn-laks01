9166a3ac35f7b77003a72812691ae4a44b232bed
Fraser Smith <smithfra@sydmac46.ozforex.local>
2020-02-05 18:00:56 +1100

    added AWS Secrets reference

--- deploy.ps1
+++ deploy.ps1
@@ -13,10 +13,9 @@
 .DESCRIPTION
   <Deploys Customer Gateways in Master Account and Peering>
 .INPUTS
-  <Modules: PS-AWS-SSO-AUTH
+  <aws exports> -- transit gateway stack name 
  # Files
-  ./files/attachment-source.yaml
-  ./files/transit-gateway.yaml>
+  ./files/customer-gateway-source.yaml"
 .OUTPUTS
   <./files/attachment.yaml>
 .NOTES
@@ -24,119 +23,108 @@
 #>
 
 #configurable variables
-$projectname = "cgw"
+$projectname = "cgw" #<--- becomes the prefix for all cgw stack names and vpn connection stacknames 
 $uuid = 'laks01'
 $region = "ap-southeast-2"
 $vpntype = 'ipsec.1'
-$transitgatewaystackname = "transit-gateway" #used to reference the transit gateway cf stackname 
+$presharekey = "testpassword000001x"
+$transitgatewaystackname = "transit-gateway" # must reference the stackname of the transit gateway cf stack for export referencing!
 
 #fixed variables
-$vpnconnectionsource = "./files/vpn-connection-source.yaml"
-$cgwsource = "./files/customer-gateway-source.yaml"
+$yamltemplate= "./files/customer-gateway-source.yaml"
+$ip=5 
 
 Write-Host "Preparing Files"
 $sites = @( 
-    [PSCustomObject]@{site="SYD"; network="10.0.0.0/16"; asn="65000"; cgw = ""; endpoints=@("49.255.71.2","14.202.31.162")},
-    [PSCustomObject]@{site="LON"; network="10.2.0.0/16"; asn="65002"; cgw = ""; endpoints=@("80.169.133.98","167.98.115.234")},
-    [PSCustomObject]@{site="SFO"; network="10.4.0.0/16"; asn="65004"; cgw = ""; endpoints=@("4.14.110.226","12.205.169.10")},
-    [PSCustomObject]@{site="TOR"; network="10.6.0.0/16"; asn="65006"; cgw = ""; endpoints=@("172.110.66.2")},
-    [PSCustomObject]@{site="HKG"; network="10.8.0.0/16"; asn="65008"; cgw = ""; endpoints=@("203.198.186.225","118.143.126.211")}
+    [PSCustomObject]@{site="syd"; network="10.0.0.0/16"; asn="65000"; cgw = ""; endpoints=@("49.255.71.2","14.202.31.162")},
+    [PSCustomObject]@{site="lon"; network="10.2.0.0/16"; asn="65002"; cgw = ""; endpoints=@("80.169.133.98","167.98.115.234")},
+    [PSCustomObject]@{site="sfo"; network="10.4.0.0/16"; asn="65004"; cgw = ""; endpoints=@("4.14.110.226","12.205.169.10")},
+    [PSCustomObject]@{site="tor"; network="10.6.0.0/16"; asn="65006"; cgw = ""; endpoints=@("172.110.66.2")},
+    [PSCustomObject]@{site="hkg"; network="10.8.0.0/16"; asn="65008" ; cgw = "" ; endpoints=@("203.198.186.225","118.143.126.211")}
 ) 
+
 Write-Host ""
 Write-Host "--------------------------------------" -f black -b green
-Write-Host " Deploying Customer Gateways and VPN." -f black -b green
+Write-Host " Deploying Customer Gateways and VPN. " -f black -b green
 Write-Host "--------------------------------------" -f black -b green
 Write-Host ""
 Write-Host ""
 
 #Create Customer Gateways
-try { $transitgatewayid = (Get-CFNExport -region $region | ? Name -like $transitgatewaystackname).Value ; Write-Host "Transit Gateway Id:" -f black -b cyan -nonewline ; Write-Host " $transitgatewayid" }
-catch { Write-Host "Transit Gateway stack does not exist, ending script" -f red ; break } #transit gateway does not exist, script will not process.
+try { $transitgatewayid = (Get-CFNExport -region $region | ? Name -like $transitgatewaystackname).Value}
+catch { Write-Host "Transit Gateway stack does not exist, ending script" -f red ; break } #checks if pre-requisite transit gateway cf deployemnt exists and breaks script if not.
+
 foreach($s in $sites){
-  
-  $endpoints = $s.endpoint
+  $x=0
+  $endpoints = $s.endpoints
   $site = $s.site
   $network = $s.network
   $asn = $s.asn
   $cgw = $s.cgw
-  $cgwfile = "./files/customer-gateway-$site.yaml"
-  $infile = $cgwsource
-  $outfile = $cgwfile
-  $stackname = "$projectname-$site"
-
-  Write-Host "Creating Customer Gateway:" -f black -b white -NoNewLine : Write-Host " $stackname"
-
-    # Find and replace
-    Write-Host "Building $infile file" -f green
-    (Get-Content $infile) | Foreach-Object {
-        $_ -replace("regexbgpasn",$asn) `
-          -replace("regexendpoint",$endpoint) `
-        } | Set-Content $outfile -force
-
-    $error.clear() ; $stack = 0 ; $stackstatus = 0
-    try { $stackstatus = ((Get-CFNStack -Stackname $stackname -region $region).StackStatus) }
-    catch { Write-Host "Stack does not exist" -f green  ; $stack = 1 }
-    
-    if($stackstatus -eq "CREATE_COMPLETE"){$stack = 0 ; Write-Host "Existing Stack Status: " -f yellow -b magenta -NoNewLine ; Write-Host "CREATE_COMPLETE... Skipping."} 
-    if($stackstatus -ne "CREATE_COMPLETE"){$stack = 2}
-
-    if($stack -eq 2){
-        # Stack exists in bad state ->>> Delete it
-        Write-Host "Removing Failed Stack"
-        Remove-CFNStack -Stackname $stackname -region $region -force  
-        Wait-CFNStack -Stackname $stackname -region $region -Status 'DELETE_COMPLETE' 
-        } 
-    if($stack -ge 1){  
-        # Create Stack
-        Write-Host "Creating Stack" -f black -b cyan
-        New-CFNStack -StackName $stackname -TemplateBody (Get-Content $outfile -raw) -Region $region
-        # Wait for Stack Deployment
-        Wait-CFNStack -Stackname $stackname -region $region
-        }
-    Write-Host ""
-    
-    Write-Host "Creating Endpoints" -f black -b white
-    $stackname = ""
+  
+    Write-Host "Creating Customer Gateways and Vpn connections" -f black -b white
     foreach($e in $endpoints){
-      $x=$x+1
-      $stackname = "$projectname-connection-0$x"
-      $infile = $vpnconnectionsource
+      $x=$x+1 ; $error.clear() ; $stack = 0 ; $stackstatus = 0
+      $stackname = "$projectname-$site-0$x"
+      $vpnconnection ="vpn-$stackname"
+      $cgwfile = "./files/customer-gateway-$site.yaml"
+      $infile = $yamltemplate
       $outfile = ".\files\$stackname.yaml"
       $endpoint = $e
-      $customergatewayid = ($s.cgw) ; if($customergatewayid -eq ""){continue}# if cgw is not configured, skip.
-      # Find and replace
-      Write-Host "Building $infile file" -f green
+      $cgw = (Get-CFNExport -region $region | ? Name -like $stackname).Value
+      $customergatewayid = ($cgw) ; if($customergatewayid -eq ""){continue}# if cgw is not configured, skip.
+      
+      #construct tunnel inside /30 ip address # 3rd octet must be between 6 and 168
+      $ip=$ip+1
+      $tunnelinside01 = "169.254."+$ip+".0/30"
+      $tunnelinside02 = "169.254."+$ip+".4/30"
+
+      # construct .yaml cf deployment file.
+      Write-Host ""
+      Write-Host "Building $outfile file" -f green
       (Get-Content $infile) | Foreach-Object {
           $_ -replace("regexvpntype",$vpntype) `
             -replace("regextgwid",$transitgatewayid) `
-            -replace("regexcgw",$cgw) `
+            -replace("regexcgw",$stackname) `
+            -replace("regexbgpasn",$asn) `
+            -replace("regexendpoint",$endpoint) `
+            -replace("regexpsk",$presharekey) `
+            -replace("regextunnelinside01",$tunnelinside01) `
+            -replace("regextunnelinside02",$tunnelinside02) `
+            -replace("regexvpnconnection",$vpnconnection)
           } | Set-Content $outfile -force
 
-      $error.clear() ; $stack = 0 ; $stackstatus = 0
+      Write-host "Validating CF Template" -f yellow -b magenta
+      Test-CFNTemplate -templateBody (Get-Content $outfile -raw) -Region $region
+      if($error.count -gt 0){Write-Host "Error Validation Failure" ; continue}
+      
       try { $stackstatus = ((Get-CFNStack -Stackname $stackname -region $region).StackStatus) }
       
-      catch { Write-Host "Stack does not exist" -f green  ; $stack = 1 }
+      catch { Write-Host "Stack does not exist!" -f yellow  ; $stack = 1 }
       
       if($stackstatus -eq "CREATE_COMPLETE"){$stack = 0 ; Write-Host "Existing Stack Status: " -f yellow -b magenta -NoNewLine ; Write-Host "CREATE_COMPLETE... Skipping."} 
-      
+      if($stackstatus -eq "CREATE_IN_PROGRESS"){Write-Host "Existing Stack Status: " -f yellow -b magenta -NoNewLine ; Write-Host "CREATE_IN_PROGRESS... Skipping."}
       if($stackstatus -ne "CREATE_COMPLETE"){$stack = 2}
 
       if($stack -eq 2){
           # Stack exists in bad state ->>> Delete it
           Write-Host "Removing Failed Stack"
           Remove-CFNStack -Stackname $stackname -region $region -force  
-          Wait-CFNStack -Stackname $stackname -region $region -Status 'DELETE_COMPLETE' 
+          Wait-CFNStack -Stackname $stackname -region $region
           } 
       if($stack -ge 1){  
           # Create Stack
           Write-Host "Creating Stack" -f black -b cyan
-          New-CFNStack -StackName $stackname -TemplateBody (Get-Content $file -raw) -Region $region
-          # Wait for Stack Deployment
-          Wait-CFNStack -Stackname $stackname -region $region
+          New-CFNStack -StackName $stackname -TemplateBody (Get-Content $outfile -raw) -Region $region
+
           }
       Write-Host ""
       }
     }
+    # Cleanup -- Remove failed deployments --- not usefull if failure status reason is needed.
+    #if($stackstatus -ne "CREATE_COMPLETE"){
+    #  Write-Host "Removing Failed Stack"
+    #  Remove-CFNStack -Stackname $stackname -region $region -force  }
 
 Write-Host ""
 Write-Host "---------------------------" -f black -b green
